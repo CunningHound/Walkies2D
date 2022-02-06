5b6c252f36d782149daa95d7ecfd49f2d043ca29
CunningHound <cunninghound@gmail.com>
2022-02-06 15:51:49 +0100

    restructuring folders

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject player;
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player != null)
+        {
+            int currentPos = (int)Mathf.Floor(10*player.transform.position.x);
+            if(currentPos > score)
+            {
+                score = currentPos;
+            }
+        }
+        
+    }
+}
