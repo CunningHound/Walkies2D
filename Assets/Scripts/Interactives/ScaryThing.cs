9a3fbbb6f52279698e8652df3c97e83782b7f152
CunningHound <cunninghound@gmail.com>
2022-02-11 16:08:18 +0100

    minimum time between scares from same scarything

--- Assets/Scripts/Interactives/ScaryThing.cs
+++ Assets/Scripts/Interactives/ScaryThing.cs
@@ -5,32 +5,51 @@ using UnityEngine;
 public class ScaryThing : MonoBehaviour
 {
     public ScaryThingType scareType;
+    public float timeBetweenScares;
+    private bool isActive;
+    private float scareCountdown;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        isActive = true;
+        scareCountdown = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isActive)
+        {
+            if (scareCountdown > 0)
+            {
+                scareCountdown -= Time.deltaTime;
+            }
+            else
+            {
+                isActive = true;
+            }
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        GameObject other = collision.gameObject;
-        PlayerController player = other.GetComponent<PlayerController>();
-        if (player != null)
+        if (isActive)
         {
-            player.React(this);
-            Activate(); 
+            GameObject other = collision.gameObject;
+            PlayerController player = other.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                player.React(this);
+                Activate();
+            }
         }
     }
 
     void Activate()
     {
+        scareCountdown = timeBetweenScares;
         Avoider avoider = gameObject.GetComponent<Avoider>();
         if(avoider != null)
         {
