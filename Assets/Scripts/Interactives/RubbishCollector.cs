bb8117e98364308b43f49d418be1ac7cdcf18689
CunningHound <cunninghound@gmail.com>
2022-03-02 20:57:21 +0100

    bag pile for rubbish collectors animation and scene updates

--- Assets/Scripts/Interactives/RubbishCollector.cs
+++ Assets/Scripts/Interactives/RubbishCollector.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class RubbishCollector : ScaryThing
 {
+    public GameObject binBagPrefab;
+    public float binBagYpos;
+    private GameObject binBags;
+
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
         scareType = ScaryThingType.RubbishCollector;
+
+        // create a pile of bin bags
+        if(binBagPrefab != null)
+        {
+            binBags = Instantiate(binBagPrefab, new Vector3(transform.position.x, binBagYpos, transform.position.z), Quaternion.identity);
+        }
     }
 
     protected override void Activate(PlayerController player)
@@ -17,5 +27,13 @@ public class RubbishCollector : ScaryThing
 
         Debug.Log("activating Rubbish Collector");
         player.React(this);
+    }
+
+    private void OnDestroy()
+    {
+        if(binBags != null)
+        {
+            Destroy(binBags);
+        }
     }
 }
